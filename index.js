caf9f0b4c220e68ef88c8f50b3424ac20eb64dd8
Manuel Peñaloza <mape_23@alu.uabcs.mx>
2025-06-15 16:53:18 -0700

    final

--- index.js
+++ index.js
@@ -3,48 +3,45 @@ const fs = require("node:fs/promises");
 const COMPARATION_COST = 1;
 const SWAP_COST = 2;
 
-let sortingAlgoritms = [
-insertionMethod,
-shellSort,
-bubleSort,
-shakerSort,
-quickSort,
-selectionSort
-];
+let sortingAlgoritms = {
+  "Insertion Method": insertionMethod,
+  "Shell Sort": shellSort,
+  "Buble Sort": bubleSort,
+  "Shaker Sort": shakerSort,
+  "Quick Sort": quickSort,
+  "Selection Sort": selectionSort
+};
 
-let files = [
-__dirname + '/data/tipicalCase:10.txt',
-__dirname + '/data/bestCase:10.txt',
-__dirname + '/data/worstCase:10.txt',
-
-__dirname + '/data/tipicalCase:100.txt',
-__dirname + '/data/bestCase:100.txt',
-__dirname + '/data/worstCase:100.txt',
-
-__dirname + '/data/tipicalCase:1000.txt',
-__dirname + '/data/bestCase:1000.txt',
-__dirname + '/data/worstCase:1000.txt',
-]
+let files = [];
+for (let c = 10; c <= 1000; c *= 10) {
+  files.push("tipicalCase:" + c);
+  files.push("bestCase:" + c);
+  files.push("worstCase:" + c);
+}
 
 test();
-
 async function test() {
-	let table = {};
-  try {
-	  for(let i = 0; i < files.length; i++){
-		let dataString = await fs.readFile(files[i], 'utf8');
-    		let dataArr = dataString.split(',');
+  let table = {};
+  let algoritms = Object.entries(sortingAlgoritms);
 
-		  for(let j = 0; j < sortingAlgoritms;j++){
-			  let tt = sortingAlgoritms[j](dataArr); 
-		  }
+  for (let i = 0; i < algoritms.length; i++) {
+    const name = algoritms[i][0];
+    let func = algoritms[i][1];
+    let currentAlgoritmData = {};
+    try {
+      for (let i = 0; i < files.length; i++) {
+        let dataString = await fs.readFile(__dirname + "/data/" + files[i] + ".txt", 'utf8');
+        let dataArr = dataString.split(',');
+        currentAlgoritmData[files[i]] = func(dataArr);
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
+    table[name] = currentAlgoritmData;
 
-
-
-	}}
-	catch (error) {
-		console.log(error);
-	}
+  }
+  console.table(table);
 }
 
 
@@ -56,57 +53,68 @@ function swap(arr, i, j) {
 }
 
 
-function selectionSort(arr){
-	console.log(arr);
-	let tt = 0;
-	for(let i =  0; i < arr.length -1; i++){
-		let minor = i;
+function selectionSort(arr) {
+  let tt = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    tt += COMPARATION_COST;
+    let minor = i;
 
-		for(let j = i +1; j < arr.length; j++){
-			tt += COMPARATION_COST;
-			if(arr[j] < arr[minor]){
-				minor = j;
-			}
-		}
+    for (let j = i + 1; j < arr.length; j++) {
+      tt += COMPARATION_COST;
 
-		tt += COMPARATION_COST;
-		if(minor != i){
-			tt += SWAP_COST;
-			swap(arr,i,minor);
-		}
+      tt += COMPARATION_COST;
+      if (arr[j] < arr[minor]) {
+        minor = j;
+      }
+    }
 
-	}
-	console.log(arr);
+    tt += COMPARATION_COST;
+    if (minor != i) {
+      swap(arr, i, minor);
+      tt += SWAP_COST;
+    }
 
+  }
+  return tt;
 }
 
 
 // quicksort partition
-function partition(arr, low, high)
-{
-    let pivot = arr[high];
-    let i = low - 1;
-    for (let j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(arr, i, j);
-        }
+function partition(arr, low, high, counter) {
+  let pivot = arr[high];
+  let i = low - 1;
+  for (let j = low; j <= high - 1; j++) {
+    counter.tt += COMPARATION_COST;
+
+    counter.tt += COMPARATION_COST;
+    if (arr[j] < pivot) {
+      i++;
+      swap(arr, i, j);
+      counter.tt += SWAP_COST;
     }
-    swap(arr, i + 1, high);
-    return i + 1;
+  }
+  swap(arr, i + 1, high);
+  counter.tt += SWAP_COST;
+  return i + 1;
 }
 
 
-function quickSort(arr,low = 0,high = 'na'){
-	if(high == 'na'){
-		high = arr.length;
-	}
+function quickSort(arr, low = 0, high = 'na') {
+  let ttCounter = {
+    tt: 0,
+  };
+  if (high == 'na') {
+    high = arr.length;
+  }
 
-    if (low < high) {
-        let pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
+  ttCounter.tt += COMPARATION_COST;
+  if (low < high) {
+    let pi = partition(arr, low, high, ttCounter);
+    ttCounter.tt += quickSort(arr, low, pi - 1);
+    ttCounter.tt += quickSort(arr, pi + 1, high);
+  }
+
+  return ttCounter.tt;
 }
 
 function shakerSort(arr) {
@@ -115,10 +123,11 @@ function shakerSort(arr) {
   let rigth = arr.length - 1;
   let k = arr.length - 1;
 
-  console.log(arr);
 
   while (left < rigth) {
+    tt += COMPARATION_COST;
     for (let i = rigth; i >= left; i--) {
+      tt += COMPARATION_COST;
       tt += COMPARATION_COST;
       if (arr[i - 1] > arr[i]) {
         swap(arr, i - 1, i);
@@ -127,20 +136,25 @@ function shakerSort(arr) {
     }
     left = k;
     for (let i = left; i <= rigth; i++) {
+      tt += COMPARATION_COST;
+      tt += COMPARATION_COST;
       if (arr[i - 1] > arr[i]) {
         swap(arr, i - 1, i);
+        tt += SWAP_COST;
         k = i;
       }
     }
     rigth = k;
   }
-  console.log(arr);
+  return tt;
 }
 
 function bubleSort(arr) {
   let tt = 0;
   for (let i = 1; i < arr.length; i++) {
+    tt += COMPARATION_COST;
     for (let j = arr.length - 1; j >= i; j--) {
+      tt += COMPARATION_COST;
       tt += COMPARATION_COST;
       if (arr[j - 1] > arr[j]) {
         swap(arr, j - 1, j);
@@ -155,8 +169,10 @@ function shellSort(arr) {
   let tt = 0;
   let interval = Math.floor(arr.length);
   while (interval > 0) {
+    tt += COMPARATION_COST;
     let sw = false;
     for (let i = 0; i + interval < arr.length; i++) {
+      tt += COMPARATION_COST;
       let j = i + interval;
       let a = arr[i];
       let b = arr[j];
@@ -179,6 +195,7 @@ function shellSort(arr) {
 function insertionMethod(arr) {
   let tt = 0;
   for (let i = 1; i < arr.length; i++) {
+    tt += COMPARATION_COST;
     let j = i;
     while (arr[j] < arr[j - 1]) {
       tt += COMPARATION_COST;
